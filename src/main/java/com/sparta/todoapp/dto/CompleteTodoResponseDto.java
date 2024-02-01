aadc73530d90a53fe59b436e6d7f0c6270d4339e
JooJuneHo <jw059@naver.com>
2024-02-01 14:50:39 +0900

    할일 작성, 선택 조회, 목록 조회, 수정, 완료 기능 구현

---
+++ src/main/java/com/sparta/todoapp/dto/CompleteTodoResponseDto.java
@@ -0,0 +1,15 @@
+package com.sparta.todoapp.dto;
+
+import com.sparta.todoapp.entity.Todo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CompleteTodoResponseDto {
+    private boolean complete;
+
+    public CompleteTodoResponseDto(Todo todo) {
+        this.complete = todo.isComplete();
+    }
+}
