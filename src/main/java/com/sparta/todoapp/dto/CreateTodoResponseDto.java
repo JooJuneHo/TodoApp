81cd7726b37cb86bac8481ca08863d3c8540aa1c
JooJuneHo <jw059@naver.com>
2024-02-01 14:41:58 +0900

    할일 작성, 선택 조회, 목록 조회, 수정, 완료 기능 구현

---
+++ src/main/java/com/sparta/todoapp/dto/CreateTodoResponseDto.java
@@ -0,0 +1,21 @@
+package com.sparta.todoapp.dto;
+
+import com.sparta.todoapp.entity.Todo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class CreateTodoResponseDto {
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+
+    public CreateTodoResponseDto(Todo todo) {
+        this.title = todo.getTitle();
+        this.description = todo.getDescription();
+        this.createdAt = todo.getCreatedAt();
+    }
+}
