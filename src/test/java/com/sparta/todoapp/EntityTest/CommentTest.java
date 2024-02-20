10eacd8152aaa68d810a8bb17b71140c23b3abb9
JooJuneHo <jw059@naver.com>
2024-02-20 15:16:14 +0900

    TestCode 추가

---
+++ src/test/java/com/sparta/todoapp/EntityTest/CommentTest.java
@@ -0,0 +1,40 @@
+package com.sparta.todoapp.EntityTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.sparta.todoapp.dto.comment.CommentRequestDto;
+import com.sparta.todoapp.entity.Comment;
+import com.sparta.todoapp.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CommentTest {
+
+    Comment comment;
+    User user;
+    CommentRequestDto commentRequestDto;
+
+    @BeforeEach
+    void set(){
+        comment = new Comment();
+        user = new User();
+    }
+
+    @Test
+    @DisplayName("Comment testComment 테스트")
+    void test1(){
+        //given
+        String title = "제목테스트";
+        String contents = "내용테스트";
+        commentRequestDto = new CommentRequestDto(title, contents);
+
+
+        //when
+        comment.Update(commentRequestDto, user);
+
+        //then
+        assertEquals(title,comment.getTitle());
+        assertEquals(contents,comment.getContents());
+    }
+}
---
+++ src/test/java/com/sparta/todoapp/TodoTest.java
@@ -0,0 +1,57 @@
+package com.sparta.todoapp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.sparta.todoapp.dto.todo.TodoRequestDto;
+import com.sparta.todoapp.entity.Todo;
+import com.sparta.todoapp.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TodoTest {
+
+
+    Todo todo;
+    User user;
+    TodoRequestDto todoRequestDto;
+
+    @BeforeEach
+    void setup(){
+        todo = new Todo();
+        user = new User();
+    }
+
+    @Test
+    @DisplayName("Todo Update 테스트")
+    void test1(){
+        //given
+        String title = "제목test";
+        String description = "내용test";
+        todoRequestDto = new TodoRequestDto(title, description);
+
+        //when
+        todo.Update(title, description);
+
+        //then
+        assertEquals(title,todo.getTitle());
+        assertEquals(description,todo.getDescription());
+
+    }
+
+    @Test
+    @DisplayName("Todo complete 테스트")
+    void test2(){
+        //given
+
+
+        //when
+        todo.Complete();
+
+        //then
+        assertTrue(todo.isComplete());
+    }
+
+}
