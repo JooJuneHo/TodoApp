33f8357741926cf371f7b12eed6f0309f8f701dc
JooJuneHo <jw059@naver.com>
2024-02-20 15:18:04 +0900

    package 변경

--- src/test/java/com/sparta/todoapp/EntityTest/TodoTest.java
+++ src/test/java/com/sparta/todoapp/EntityTest/TodoTest.java
@@ -1,4 +1,4 @@
-package com.sparta.todoapp;
+package com.sparta.todoapp.EntityTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
